07a7edb08c092c922641761097c97555644c21fd
Eládio Leal <Eladiolink1@gmail.com>
2023-01-03 17:45:39 -0300

    Finalização do Insert e Delete na AVL Tree

--- AvlTree.c
+++ AvlTree.c
@@ -9,53 +9,8 @@ struct Tree
 	int info;
 	Tree * left;
 	Tree * right;
-	int bal;
+	int height;
 };
-
-Tree * creatTree(int info){
-	Tree * t = (Tree *) malloc(sizeof(Tree));
-
-	if(t){
-		t->info = info;
-		t->bal = 0;
-		t->left = NULL;
-		t->right = NULL;
-		t->parent = NULL;
-		return t;
-	}
-
-	return NULL;
-}
-
-Tree * insert(Tree **t, int value){
-	if(!*t)	*t = creatTree(value);
-
-	Tree *new = creatTree(value);
-
-	Tree * p = *t;
-	Tree * q = NULL;
-	while (p!=NULL)
-	{
-		q = p;
-		if(p->info == new->info) return p;
-
-		if(p->info > new->info){
-			p = p->left;
-		}else{
-			p = p->right;
-		}
-	}
-	
-	if(t==NULL){
-		*t = new;
-	}else{
-		if(q->info > new->info){
-			q->left = new;
-		}else{
-			q->right = new;
-		}
-	}
-}
 
 void printTree(Tree * root, int level) {
   if (root == NULL) return;
@@ -68,23 +23,184 @@ void printTree(Tree * root, int level) {
   printTree(root->left, level+1);
 }
 
-void printTreeV(Tree * root, int level) {
-  if (root == NULL) return;
+Tree * creatTree(int info){
+	Tree * t = (Tree *) malloc(sizeof(Tree));
 
-  printTree(root->right, level+1);
-  printf("%*d\n", level*4, root->info);
-  printTree(root->left, level+1);
+	if(t){
+		t->info = info;
+		t->height = 0;
+		t->left = NULL;
+		t->right = NULL;
+		t->parent = NULL;
+		return t;
+	}
+
+	return NULL;
 }
+
+// retorna a maior valor entre a e b
+
+int maior(int a, int b){
+	return (a>b)? a : b;
+}
+
+int alturaDoNo(Tree * n){
+	if(!n) return -1;
+
+	return n->height;
+}
+
+//Calcula e retorna o fator de balanceamento de um nó
+
+int fatorDeBalanceamento(Tree * r){
+	if(r)
+		return (alturaDoNo(r->left) - alturaDoNo(r->right));
+	return 0; 
+}
+
+Tree * RR(Tree * r){ // Rotação da Direita
+	Tree *y, *f;
+
+	y = r->right;
+	f = y->left;
+    
+	y->left = r;
+	r->right = f;
+
+    r->height = maior(alturaDoNo(r->left),alturaDoNo(r->right)+1);
+	y->height = maior(alturaDoNo(y->left),alturaDoNo(y->right)+1);
+
+	return y;
+}
+
+Tree * LL(Tree * r){ // Rotação a Esqueda
+	Tree *y, *f;
+
+	y = r->left;
+	f = y->right;
+    
+	y->right = r;
+	r->left = f;
+
+    r->height = maior(alturaDoNo(r->left),alturaDoNo(r->right)+1);
+	y->height = maior(alturaDoNo(y->left),alturaDoNo(y->right)+1);
+
+	return y;
+}
+
+Tree * RL(Tree * r){ // Rotação Dupla a Direita Esqueda
+	r->right = LL(r->right);
+	return RR(r);
+}
+
+Tree * LR(Tree * r){ // Rotação Dupla a Esqueda Direita
+	r->left =RR(r->left);
+	return LL(r);
+}		
+
+Tree * balancear(Tree * r){
+
+	int fb = fatorDeBalanceamento(r);
+
+	if(fb< -1 && fatorDeBalanceamento(r->right)<=0)
+			r = RR(r);
+	else if(fb > 1 && fatorDeBalanceamento(r->left)>=0)
+			r = LL(r);
+		
+	else if(fb > 1 && fatorDeBalanceamento(r->left)<0)
+			r = LR(r); 
+
+	else if(fb < -1 && fatorDeBalanceamento(r->right)>0)
+			r = RL(r);
+
+	 return r;
+}
+
+Tree * insert(Tree * r,int value){
+	// Adição
+	if(r == NULL){
+		return creatTree(value);
+	}
+
+	if(r->info == value){
+		printf("NÓ JÁ INSERIDO");
+		return r;
+	}
+
+	if(value < r->info){
+		r->left = insert(r->left,value);
+	}else{
+		r->right = insert(r->right,value);
+	}
+
+	r->height = maior(alturaDoNo(r->left),alturaDoNo(r->right)) +1;
+
+	// Balanceamento
+	r = balancear(r);
+
+	return r;
+}
+
+Tree * removeTree(Tree *r,int value){
+	if(r->info == value){
+		if(r->left == NULL && r->right==NULL){
+			free(r);
+			printf("Folha Removido");
+			return NULL;
+		}else if(r->left != NULL && r->right != NULL){
+			Tree * q = r->left;
+			Tree * aux= NULL;
+			Tree * x = NULL;
+			while (q){
+			 x = aux;	
+			 aux = q;
+			 q = q->right;
+			}
+
+			r->info = aux->info;
+
+			free(aux);
+			x->right = NULL;
+			return r;
+		}else{
+			return (r->left) ?  r->left : r->right;
+		}
+
+	}else{
+		if(r->info > value){
+			r->left = removeTree(r->left,value);
+		}else{
+			r->right = removeTree(r->right,value);
+		}
+	}
+
+	r->height = maior(alturaDoNo(r->left),alturaDoNo(r->right))+1;
+
+	r = balancear(r);
+	return r;
+}
+
 int main(){
 	Tree * t = NULL;
 	
-	insert(&t,40);
-	insert(&t,20);
-	insert(&t,10);
-	insert(&t,25);
+	t = insert(t,40);
+	t = insert(t,44);
+	t = insert(t,46);
+	t = insert(t,13);
+	t = insert(t,9);
+    t = insert(t,16);
+	t = insert(t,15);
+	t = insert(t,14);
+	t = insert(t,2);
 
+	printf("\n\n");
 	printTree(t,0);
-
 	printf("\n");
+
+	t = removeTree(t,9);
+	printf("\n\n");
+	printTree(t,0);
+	printf("\n");
+
     return 0;
 }
